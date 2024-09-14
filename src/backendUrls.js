05b6cb871fa04f051168458d87e0d5e5df3fb4e4
ninja-scripter <henrywinslow9519@gmail.com>
2024-09-14 11:59:25 +0900

    initial mvp

---
+++ src/backendUrls.js
@@ -0,0 +1,11 @@
+// const backendUrls = {
+//   http: 'https://leschatapp.herokuapp.com',
+//   ws: 'wss://leschatapp.herokuapp.com/graphql',
+// };
+
+const backendUrls = {
+  http: "http://localhost:4000",
+  ws: "ws://localhost:4000/graphql"
+}
+
+export default backendUrls;
